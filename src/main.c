a02ba118ab8037784d31518591e0ad3b74bdfa76
dianlight <lucio.tarantino@gmail.com>
2020-07-27 21:01:58 +0200

    Camera e Stream Funzionanti!

--- src/main.c
+++ src/main.c
@@ -11,8 +11,252 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+//#include "camera_pin.h"
+
+
+#include "esp_camera.h"
+#include "esp_http_server.h"
+#include "esp_timer.h"
+#include <esp_wifi.h>
+#include <esp_event_loop.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+
+
+
+static const char *TAG = "example:take_picture";
+
+static camera_config_t camera_config = {
+    .pin_pwdn = CONFIG_PWDN,
+    .pin_reset = CONFIG_RESET,
+    .pin_xclk = CONFIG_XCLK,
+    .pin_sscb_sda = CONFIG_SDA,
+    .pin_sscb_scl = CONFIG_SCL,
+
+    .pin_d7 = CONFIG_D7,
+    .pin_d6 = CONFIG_D6,
+    .pin_d5 = CONFIG_D5,
+    .pin_d4 = CONFIG_D4,
+    .pin_d3 = CONFIG_D3,
+    .pin_d2 = CONFIG_D2,
+    .pin_d1 = CONFIG_D1,
+    .pin_d0 = CONFIG_D0,
+    .pin_vsync = CONFIG_VSYNC,
+    .pin_href = CONFIG_HREF,
+    .pin_pclk = CONFIG_PCLK,
+
+    //XCLK 20MHz or 10MHz
+    .xclk_freq_hz = CONFIG_XCLK_FREQ,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_UXGA,   //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+
+    .jpeg_quality = 12, //0-63 lower number means higher quality
+    .fb_count = 1       //if more than one, i2s runs in continuous mode. Use only with JPEG
+};
+
+// Camera
+static esp_err_t init_camera()
+{
+//    //power up the camera if PWDN pin is defined
+//    if(CAM_PIN_PWDN != -1){
+//        gpio_pad_select_gpio(CAM_PIN_PWDN);
+//        gpio_set_direction(CAM_PIN_PWDN,GPIO_MODE_OUTPUT);
+//        gpio_set_level(CAM_PIN_PWDN, 0);
+//    }
+
+    //initialize the camera
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Camera Init Failed 0x%x",err);
+        return err;
+    }
+
+    return ESP_OK;
+}
+
+
+// HTTP 
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char* _STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char* _STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char* _STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+esp_err_t jpg_stream_httpd_handler(httpd_req_t *req){
+    camera_fb_t * fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len;
+    uint8_t * _jpg_buf;
+    char * part_buf[64];
+    static int64_t last_frame = 0;
+    if(!last_frame) {
+        last_frame = esp_timer_get_time();
+    }
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if(res != ESP_OK){
+        return res;
+    }
+
+    while(true){
+        fb = esp_camera_fb_get();
+        if (!fb) {
+            ESP_LOGE(TAG, "Camera capture failed");
+            res = ESP_FAIL;
+            break;
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+            if(!jpeg_converted){
+                ESP_LOGE(TAG, "JPEG compression failed");
+                esp_camera_fb_return(fb);
+                res = ESP_FAIL;
+            }
+        } else {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if(res == ESP_OK){
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if(res == ESP_OK){
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if(fb->format != PIXFORMAT_JPEG){
+            free(_jpg_buf);
+        }
+        esp_camera_fb_return(fb);
+        if(res != ESP_OK){
+            break;
+        }
+        int64_t fr_end = esp_timer_get_time();
+        int64_t frame_time = fr_end - last_frame;
+        last_frame = fr_end;
+        frame_time /= 1000;
+        ESP_LOGI(TAG, "MJPG: %uKB %ums (%.1ffps)",
+            (uint32_t)(_jpg_buf_len/1024),
+            (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+    }
+
+    last_frame = 0;
+    return res;
+}
+
+// WebServer and WiFi
+httpd_uri_t uri_handler_jpg = {
+    .uri = "/jpg",
+    .method = HTTP_GET,
+    .handler = jpg_stream_httpd_handler};
+
+httpd_handle_t start_webserver(void)
+{
+  httpd_handle_t server = NULL;
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+
+  // Start the httpd server
+  ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+  if (httpd_start(&server, &config) == ESP_OK)
+  {
+    // Set URI handlers
+    ESP_LOGI(TAG, "Registering URI handlers");
+    httpd_register_uri_handler(server, &uri_handler_jpg);
+    return server;
+  }
+
+  ESP_LOGI(TAG, "Error starting server!");
+  return NULL;
+}
+
+void stop_webserver(httpd_handle_t server)
+{
+  // Stop the httpd server
+  httpd_stop(server);
+}
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+  httpd_handle_t *server = (httpd_handle_t *)ctx;
+
+  switch (event->event_id)
+  {
+  case SYSTEM_EVENT_STA_START:
+    ESP_LOGI(TAG, "SYSTEM_EVENT_STA_START");
+    ESP_ERROR_CHECK(esp_wifi_connect());
+    break;
+  case SYSTEM_EVENT_STA_GOT_IP:
+    ESP_LOGI(TAG, "SYSTEM_EVENT_STA_GOT_IP");
+    ESP_LOGI(TAG, "Got IP: '%s'",
+             ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+
+    /* Start the web server */
+    if (*server == NULL)
+    {
+      *server = start_webserver();
+    }
+    break;
+  case SYSTEM_EVENT_STA_DISCONNECTED:
+    ESP_LOGI(TAG, "SYSTEM_EVENT_STA_DISCONNECTED");
+    ESP_ERROR_CHECK(esp_wifi_connect());
+
+    /* Stop the web server */
+    if (*server)
+    {
+      stop_webserver(*server);
+      *server = NULL;
+    }
+    break;
+  default:
+    break;
+  }
+  return ESP_OK;
+}
+
+static void initialise_wifi(void *arg)
+{
+  tcpip_adapter_init();
+  ESP_ERROR_CHECK(esp_event_loop_init(event_handler, arg));
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+
+  wifi_config_t wifi_config = {
+      .sta = {
+          .ssid = CONFIG_WIFI_SSID,
+          .password = CONFIG_WIFI_PASSWORD,
+      },
+  };
+  
+  ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+
+// Main
 void app_main(void)
 {
+
+    esp_log_level_set("*", ESP_LOG_WARN);        // set all components to ERROR level
+    esp_log_level_set("spiram", ESP_LOG_WARN);
+    esp_log_level_set("efuse", ESP_LOG_WARN);
+    esp_log_level_set("wifi*", ESP_LOG_WARN);      // enable WARN logs from WiFi stack
+    esp_log_level_set("wpa", ESP_LOG_WARN);      // enable WARN logs from WiFi stack
+    esp_log_level_set("httpd_parse", ESP_LOG_INFO);     // enable INFO logs from DHCP client
+    esp_log_level_set("camera", ESP_LOG_DEBUG);     // enable INFO logs from DHCP client
+    esp_log_level_set("example:take_picture", ESP_LOG_DEBUG);     // enable INFO logs from DHCP client
+
     printf("Hello world!\n");
 
     /* Print chip information */
@@ -31,6 +275,16 @@ void app_main(void)
 
     printf("Free heap: %d\n", esp_get_free_heap_size());
 
+    printf("Free heap size: %d\n", (int) xPortGetFreeHeapSize());
+//    heap_caps_print_heap_info(MALLOC_CAP_DEFAULT);
+
+ 
+    static httpd_handle_t server = NULL;
+    ESP_ERROR_CHECK(nvs_flash_init());
+    init_camera();
+    initialise_wifi(&server);
+
+/*
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
@@ -38,4 +292,5 @@ void app_main(void)
     printf("Restarting now.\n");
     fflush(stdout);
     esp_restart();
+*/    
 }
