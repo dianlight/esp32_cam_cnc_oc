3625baa05424e6682d81cd9b9912f1af2f6b960c
dianlight <lucio.tarantino@gmail.com>
2020-07-28 08:48:42 +0200

    MCP23x17 ok! SDMMC ok! ADS111x working!

--- src/main.c
+++ src/main.c
@@ -4,7 +4,12 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+
+#define SDA_GPIO 13
+#define SCL_GPIO 12
+
 #include <stdio.h>
+#include <dirent.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -22,6 +27,15 @@
 #include <sys/param.h>
 #include <u8g2.h>
 #include <u8g2_esp32_hal.h>
+#include <i2cdev.h>
+#include <mcp23x17.h>
+#include <ads111x.h>
+
+#include "esp_vfs_fat.h"
+#include "sdmmc_cmd.h"
+#include "driver/sdmmc_host.h"
+
+#define MOUNT_POINT "/sdcard"
 
 
 static const char *TAG = "example:take_picture";
@@ -243,6 +257,192 @@ static void initialise_wifi(void *arg)
 
 //U8G2_SSD1309_128X64_NONAME2_2_HW_I2C u8g2(U8G2_R0, /*reset*/ U8X8_PIN_NONE, /* clock */ 12, /* data */ 13);
 
+void buttonsTask(void *pvParameters)
+{
+    mcp23x17_t dev;
+    memset(&dev, 0, sizeof(mcp23x17_t));
+    ESP_ERROR_CHECK(mcp23x17_init_desc(&dev, 0, MCP23X17_ADDR_BASE, SDA_GPIO, SCL_GPIO));
+    
+
+    // Setup PORTA0 as input
+    for(u8_t p=0; p < 8; p++){
+         mcp23x17_set_mode(&dev, p, MCP23X17_GPIO_INPUT);
+    }
+    mcp23x17_port_set_pullup(&dev, 0x00FF);
+
+/*         
+    // Setup interrupt on it
+    mcp23x17_set_interrupt(&dev, 0, MCP23X17_INT_ANY_EDGE);
+
+    gpio_set_direction(INTA_GPIO, GPIO_MODE_INPUT);
+    gpio_set_intr_type(INTA_GPIO, GPIO_INTR_ANYEDGE);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(INTA_GPIO, intr_handler, NULL);
+
+    // Setup PORTB0 as output
+    mcp23x17_set_mode(&dev, 8, MCP23X17_GPIO_OUTPUT);
+    // do some blinkning
+    bool on = true;
+    while (1)
+    {
+        mcp23x17_set_level(&dev, 8, on);
+        on = !on;
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+*/
+
+    i2c_dev_t device;
+    memset(&device, 0, sizeof(i2c_dev_t));
+
+    ESP_ERROR_CHECK(ads111x_init_desc(&device, ADS111X_ADDR_GND, 0, SDA_GPIO, SCL_GPIO));
+    ESP_ERROR_CHECK(ads111x_set_mode(&device, ADS111X_MODE_CONTUNOUS));    
+    ESP_ERROR_CHECK(ads111x_set_comp_mode(&device, ADS111X_COMP_MODE_WINDOW));    
+    ESP_ERROR_CHECK(ads111x_set_data_rate(&device, ADS111X_DATA_RATE_8)); // 32 samples per second
+    ESP_ERROR_CHECK(ads111x_set_gain(&device, ADS111X_GAIN_4V096));
+
+
+
+    while(1)
+    {
+        uint32_t val = 0;
+        for(u8_t p=0; p < 8; p++){
+            if (mcp23x17_get_level(&dev,p,&val) == ESP_OK)
+            {
+                printf("%c",!val?'1':'0');
+                if(p == 0 && !val){
+                    printf("\nFree heap size: %d\n", (int) xPortGetFreeHeapSize());
+                }
+            } else
+                printf("*");
+        }
+        /*
+        printf("%c%c%c%c%c%c%c%c ",
+                  !mcp23x17_get_level(&dev,0,true) ? 'J' : '0',  // JOYSTICK 
+                  !mcp23x17_get_level(&dev,1,true) ? '2' : '0', 
+                  !mcp23x17_get_level(&dev,2,true) ? '3' : '0',
+                  !mcp23x17_get_level(&dev,3,true) ? 'N' : '0',  // NEXT
+                  !mcp23x17_get_level(&dev,4,true) ? 'B' : '0',  // BEFORE
+                  !mcp23x17_get_level(&dev,5,true) ? 'I' : '0',  // INC
+                  !mcp23x17_get_level(&dev,6,true) ? 'D' : '0',  // DEC 
+                  !mcp23x17_get_level(&dev,7,true) ? 'S' : '0'); // SEL
+        */       
+        ESP_ERROR_CHECK(ads111x_set_input_mux(&device, ADS111X_MUX_0_GND));    // positive = AIN0, negative = GND 
+        ESP_ERROR_CHECK(ads111x_start_conversion(&device));   
+        bool busy = true;
+        do
+        {
+            ESP_ERROR_CHECK(ads111x_is_busy(&device, &busy));
+        }
+        while (busy);
+
+        int16_t raw = 0;
+        if (ads111x_get_value(&device, &raw) == ESP_OK)
+        {
+        //    float voltage = ads111x_gain_values[ADS111X_GAIN_4V096] / ADS111X_MAX_VALUE * raw;
+        //    printf("Raw ADC value: %d, voltage: %.04f volts\n", raw, voltage);
+              printf("Raw ADC value: %d,", raw);
+        }
+        else
+            printf("Cannot read ADC value,");
+
+        vTaskDelay(10 / portTICK_PERIOD_MS);    
+
+        busy = true;
+        ESP_ERROR_CHECK(ads111x_set_input_mux(&device, ADS111X_MUX_2_GND));    // positive = AIN0, negative = GND 
+        ESP_ERROR_CHECK(ads111x_start_conversion(&device));  
+        do
+        {
+            ESP_ERROR_CHECK(ads111x_is_busy(&device, &busy));
+        }
+        while (busy);
+
+        if (ads111x_get_value(&device, &raw) == ESP_OK)
+        {
+        //    float voltage = ads111x_gain_values[ADS111X_GAIN_4V096] / ADS111X_MAX_VALUE * raw;
+        //    printf("Raw ADC value: %d, voltage: %.04f volts\n", raw, voltage);
+              printf("Raw ADC value: %d\n", raw);
+        }
+        else
+            printf("Cannot read ADC value\n");
+
+         vTaskDelay(300 / portTICK_PERIOD_MS);
+    }    
+}
+
+// SSD READER
+void ssdtest(void)
+{
+    esp_vfs_fat_sdmmc_mount_config_t mount_config = {
+        .format_if_mount_failed = false,
+        .max_files = 5,
+        .allocation_unit_size = 16 * 1024
+    };
+    sdmmc_card_t* card;
+    const char mount_point[] = MOUNT_POINT;
+    ESP_LOGI(TAG, "Initializing SD card");
+
+    ESP_LOGI(TAG, "Using SDMMC peripheral");
+    sdmmc_host_t host = SDMMC_HOST_DEFAULT();
+
+    // This initializes the slot without card detect (CD) and write protect (WP) signals.
+    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
+    sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
+
+    // To use 1-line SD mode, uncomment the following line:
+    slot_config.width = 1;
+
+    // GPIOs 15, 2, 4, 12, 13 should have external 10k pull-ups.
+    // Internal pull-ups are not sufficient. However, enabling internal pull-ups
+    // does make a difference some boards, so we do that here.
+    /*
+    gpio_set_pull_mode(15, GPIO_PULLUP_ONLY);   // CMD, needed in 4- and 1- line modes
+    gpio_set_pull_mode(2, GPIO_PULLUP_ONLY);    // D0, needed in 4- and 1-line modes
+    gpio_set_pull_mode(4, GPIO_PULLUP_ONLY);    // D1, needed in 4-line mode only
+    gpio_set_pull_mode(12, GPIO_PULLUP_ONLY);   // D2, needed in 4-line mode only
+    gpio_set_pull_mode(13, GPIO_PULLUP_ONLY);   // D3, needed in 4- and 1-line modes
+    */
+
+    esp_err_t ret;
+    ret = esp_vfs_fat_sdmmc_mount(mount_point, &host, &slot_config, &mount_config, &card);
+
+     if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount filesystem. "
+                "If you want the card to be formatted, set the EXAMPLE_FORMAT_IF_MOUNT_FAILED menuconfig option.");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize the card (%s). "
+                "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
+        }
+        return;
+    }
+
+    // Card has been initialized, print its properties
+    sdmmc_card_print_info(stdout, card);
+
+    // Stuff
+    ESP_LOGI(TAG,"Listing root directory: \n");
+
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(MOUNT_POINT"/");
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    }
+
+
+
+    // All done, unmount partition and disable SDMMC or SPI peripheral
+//    esp_vfs_fat_sdcard_unmount(mount_point, card);
+    esp_vfs_fat_sdmmc_unmount();
+    ESP_LOGI(TAG, "Card unmounted");
+}
+
+
 // Main
 void app_main(void)
 {
@@ -286,8 +486,8 @@ void app_main(void)
 
 
     u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
-	u8g2_esp32_hal.sda   = 13; //PIN_SDA;
-	u8g2_esp32_hal.scl  = 12; //PIN_SCL;
+	u8g2_esp32_hal.sda   = SDA_GPIO; 
+	u8g2_esp32_hal.scl  = SCL_GPIO; 
 	u8g2_esp32_hal_init(u8g2_esp32_hal);
 
 
@@ -324,6 +524,14 @@ void app_main(void)
 
 	ESP_LOGI(TAG, "All done!");
     printf("Free heap size: %d\n", (int) xPortGetFreeHeapSize());
+
+    ssdtest();
+    printf("Free heap size: %d\n", (int) xPortGetFreeHeapSize());
+
+    ESP_ERROR_CHECK(i2cdev_init());
+   // xTaskCreate(buttonsTask, "buttonsTask", configMINIMAL_STACK_SIZE * 6, NULL, 5, NULL);
+    xTaskCreatePinnedToCore(buttonsTask, "buttonsTask", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL, APP_CPU_NUM);
+
 
 /*
 
