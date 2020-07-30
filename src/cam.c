b56938724b37e83f605affebf2d58ee4d7f4d93c
dianlight <lucio.tarantino@gmail.com>
2020-07-31 00:59:18 +0200

    Code Cleanup

---
+++ src/cam.c
@@ -0,0 +1,139 @@
+#include "cam.h"
+#include <esp_camera.h>
+#include "esp_log.h"
+
+
+
+static const char *TAG = "cam";
+
+static camera_config_t camera_config = {
+    .pin_pwdn = CONFIG_PWDN,
+    .pin_reset = CONFIG_RESET,
+    .pin_xclk = CONFIG_XCLK,
+    .pin_sscb_sda = CONFIG_SDA,
+    .pin_sscb_scl = CONFIG_SCL,
+
+    .pin_d7 = CONFIG_D7,
+    .pin_d6 = CONFIG_D6,
+    .pin_d5 = CONFIG_D5,
+    .pin_d4 = CONFIG_D4,
+    .pin_d3 = CONFIG_D3,
+    .pin_d2 = CONFIG_D2,
+    .pin_d1 = CONFIG_D1,
+    .pin_d0 = CONFIG_D0,
+    .pin_vsync = CONFIG_VSYNC,
+    .pin_href = CONFIG_HREF,
+    .pin_pclk = CONFIG_PCLK,
+
+    //XCLK 20MHz or 10MHz
+    .xclk_freq_hz = CONFIG_XCLK_FREQ,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG, //YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size = FRAMESIZE_UXGA,   //QQVGA-UXGA Do not use sizes above QVGA when not JPEG
+
+    .jpeg_quality = 12, //0-63 lower number means higher quality
+    .fb_count = 1       //if more than one, i2s runs in continuous mode. Use only with JPEG
+};
+
+
+// Camera
+esp_err_t init_camera()
+{
+    //initialize the camera
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Camera Init Failed 0x%x", err);
+        return err;
+    }
+
+    return ESP_OK;
+}
+
+// HTTP
+#define PART_BOUNDARY "123456789000000000000987654321"
+static const char *_STREAM_CONTENT_TYPE = "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART = "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+esp_err_t jpg_stream_httpd_handler(httpd_req_t *req)
+{
+    camera_fb_t *fb = NULL;
+    esp_err_t res = ESP_OK;
+    size_t _jpg_buf_len;
+    uint8_t *_jpg_buf;
+    char *part_buf[64];
+    static int64_t last_frame = 0;
+    if (!last_frame)
+    {
+        last_frame = esp_timer_get_time();
+    }
+
+    res = httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+    if (res != ESP_OK)
+    {
+        return res;
+    }
+
+    while (true)
+    {
+        fb = esp_camera_fb_get();
+        if (!fb)
+        {
+            ESP_LOGE(TAG, "Camera capture failed");
+            res = ESP_FAIL;
+            break;
+        }
+        if (fb->format != PIXFORMAT_JPEG)
+        {
+            bool jpeg_converted = frame2jpg(fb, 80, &_jpg_buf, &_jpg_buf_len);
+            if (!jpeg_converted)
+            {
+                ESP_LOGE(TAG, "JPEG compression failed");
+                esp_camera_fb_return(fb);
+                res = ESP_FAIL;
+            }
+        }
+        else
+        {
+            _jpg_buf_len = fb->len;
+            _jpg_buf = fb->buf;
+        }
+
+        if (res == ESP_OK)
+        {
+            res = httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+        }
+        if (res == ESP_OK)
+        {
+            size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, _jpg_buf_len);
+
+            res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+        }
+        if (res == ESP_OK)
+        {
+            res = httpd_resp_send_chunk(req, (const char *)_jpg_buf, _jpg_buf_len);
+        }
+        if (fb->format != PIXFORMAT_JPEG)
+        {
+            free(_jpg_buf);
+        }
+        esp_camera_fb_return(fb);
+        if (res != ESP_OK)
+        {
+            break;
+        }
+        int64_t fr_end = esp_timer_get_time();
+        int64_t frame_time = fr_end - last_frame;
+        last_frame = fr_end;
+        frame_time /= 1000;
+        ESP_LOGI(TAG, "MJPG: %uKB %ums (%.1ffps)",
+                 (uint32_t)(_jpg_buf_len / 1024),
+                 (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
+    }
+
+    last_frame = 0;
+    return res;
+}
