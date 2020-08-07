c94a6eae51325ab85954d4856f835dc4747447b8
dianlight <lucio.tarantino@gmail.com>
2020-08-07 16:55:13 +0200

    + udp_logging component

---
+++ components/udp_logging/udp_logging.c
@@ -0,0 +1,138 @@
+//  Copyright 2017 by Malte Janduda
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+#include "udp_logging.h"
+
+#include "esp_system.h"
+#include "esp_log.h"
+
+#include <string.h>
+
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+
+int udp_log_fd;
+static struct sockaddr_in serveraddr;
+static uint8_t buf[UDP_LOGGING_MAX_PAYLOAD_LEN];
+static vprintf_like_t ovprintf = vprintf;
+
+int get_socket_error_code(int socket)
+{
+	int result;
+	u32_t optlen = sizeof(int);
+	if(getsockopt(socket, SOL_SOCKET, SO_ERROR, &result, &optlen) == -1) {
+	printf("getsockopt failed");
+	return -1;
+	}
+	return result;
+}
+
+int show_socket_error_reason(int socket)
+{
+	int err = get_socket_error_code(socket);
+	printf("UDP socket error %d %s", err, strerror(err));
+	return err;
+}
+
+void udp_logging_free(va_list l) {
+	int err = 0;
+	char *err_buf;
+    esp_log_set_vprintf(ovprintf);
+    if( (err = shutdown(udp_log_fd, 2)) == 0 )
+	{
+		ovprintf("\nUDP socket shutdown!", l);
+	}else
+	{
+    	asprintf(&err_buf, "\nShutting-down UDP socket failed: %d!\n", err);
+		ovprintf(err_buf, l);
+	}
+
+    if( (err = close( udp_log_fd )) == 0 )
+    {
+		ovprintf("\nUDP socket closed!", l);
+	}else
+	{
+		asprintf(&err_buf, "\n Closing UDP socket failed: %d!\n", err);
+		ovprintf(err_buf, l);
+	}
+    udp_log_fd = 0;
+}
+
+
+int udp_logging_vprintf( const char *str, va_list l ) {
+    int err = 0;
+	int len = 0;
+	char task_name[16];
+	char *cur_task = pcTaskGetTaskName(xTaskGetCurrentTaskHandle());
+	strncpy(task_name, cur_task, 16);
+	task_name[15] = 0;
+	if (strncmp(task_name, "tiT", 16) != 0)
+	{
+		len = vsprintf((char*)buf, str, l);
+		if( (err = sendto(udp_log_fd, buf, len, 0, (struct sockaddr *)&serveraddr, sizeof(serveraddr))) < 0 )
+		{
+			#ifdef UDP_LOG_FALLBACK_MODE
+				show_socket_error_reason(udp_log_fd);
+				ovprintf("\nFreeing UDP Logging. sendto failed!\n", l);
+				udp_logging_free(l);
+				return ovprintf("UDP Logging freed!\n\n", l);
+			#endif 
+		}
+	}
+	#ifdef CONFIG_UDP_LOG_MIRROR_MODE
+		return ovprintf( str, l );
+	#else
+		return len;
+	#endif 
+}
+
+int udp_logging_init(const char *ipaddr, unsigned long port, vprintf_like_t func, vprintf_like_t fallback ) {
+	if(fallback != NULL)ovprintf = fallback;
+	struct timeval send_timeout = {1,0};
+	udp_log_fd = 0;
+	ESP_LOGI("UDP_LOGGING", "initializing udp logging...");
+    if( (udp_log_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP)) < 0 ) {
+       ESP_LOGE("UDP_LOGGING", "Cannot open socket!");
+       return -1;
+    }
+
+    uint32_t ip_addr_bytes;
+    inet_aton(ipaddr, &ip_addr_bytes);
+    ESP_LOGI("UDP_LOGGING", "Logging to 0x%x", ip_addr_bytes);
+
+    memset( &serveraddr, 0, sizeof(serveraddr) );
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons( port );
+    serveraddr.sin_addr.s_addr = ip_addr_bytes;
+
+    int err = setsockopt(udp_log_fd, SOL_SOCKET, SO_SNDTIMEO, (const char *)&send_timeout, sizeof(send_timeout));
+	if (err < 0) {
+	   ESP_LOGE("UDP_LOGGING", "Failed to set SO_SNDTIMEO. Error %d", err);
+	}
+	
+	int broadcast = 1;
+	err = setsockopt( udp_log_fd, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof(broadcast) );
+	if (err < 0) {
+	   ESP_LOGE("UDP_LOGGING", "Failed to set SO_BROADCAST. Error %d", err);
+	}
+
+    esp_log_set_vprintf(func);
+
+    return 0;
+}
+
+
